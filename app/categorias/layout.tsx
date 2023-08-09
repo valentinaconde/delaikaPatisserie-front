8e3d603e23d4b79d665a164480cf16c47d5e97f8
Valentina Conde <68123729+valentinaconde@users.noreply.github.com>
2023-08-09 15:36:04 -0300

    get categories layout

--- app/categorias/layout.tsx
+++ app/categorias/layout.tsx
@@ -1,7 +1,8 @@
-import './globals.scss'
+
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import Navbar from 'app/components/navbar'
+import Categories from 'app/components/categories'
+import styles from './layout.module.scss'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,14 +16,11 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className} style={{
-        margin: '0',
-        padding: '0',
-      }}>
-        <Navbar />
-        {children}
-      </body>
-    </html>
+
+ <div className={styles.container}>
+ <Categories />
+ <div>{children}</div>
+ </div>
+     
   )
 }
