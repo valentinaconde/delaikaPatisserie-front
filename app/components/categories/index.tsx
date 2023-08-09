1d509fe31523de4fdf3a4d24efb0f36e83eb8979
Valentina Conde <68123729+valentinaconde@users.noreply.github.com>
2023-08-09 15:54:02 -0300

    set category selected

--- app/components/categories/index.tsx
+++ app/components/categories/index.tsx
@@ -2,15 +2,21 @@
 import { useState } from 'react';
 import styles from './categories.module.scss'
 import categories from '@/app/utils/categories.utils';
+import { useParams } from 'next/navigation';
 
 export default function Categories() {
+
+
+  const params = useParams()
+
   return (
     <div className={styles.container}>
-        <h1>CATEGORIAS</h1>
-        {categories.map((category, index) => (
-            <a href={`/categorias/${category.id}`} key={index} className={styles.category}>{category.name}</a>
-        ))
-        }
+      <h1>CATEGORIAS</h1>
+      {categories.map((category, index) => (
+        <a href={`/categorias/${category.id}`} key={index} className={category.id === Number(params.id) ?
+          `${styles.category} ${styles.category2}` : styles.category}>{category.name}</a>
+      ))
+      }
     </div>
   )
 }
